20f6c82a5bb75c689751ad68f8878739aefcc814
Aliansandr Makavetski <aliaksandr.makavetski@gmail.com>
2016-08-21 16:41:20 +0300

    -stage 2

--- src/main/java/quoters/Main.java
+++ src/main/java/quoters/Main.java
@@ -2,17 +2,10 @@ package quoters;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-/**
- * Created by Aliaksandr_Makavets1 on 19.08.2016.
- */
-public class Main
-{
-    public static void main(String[] args) throws InterruptedException {
+
+public class Main {
+    public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "context.xml");
-        while (true) {
-            Thread.sleep(100);
-            context.getBean(Quoter.class).sayQoute();
-        }
     }
 }
--- src/main/java/quoters/ProfilerController.java
+++ src/main/java/quoters/ProfilerController.java
@@ -1,7 +1,7 @@
 package quoters;
 
 public class ProfilerController implements ProfilerControllerMBean{
-    private boolean enabled;
+    private boolean enabled = true;
 
     public boolean isEnabled() {
         return enabled;
